76d2f5b779929ac3458928a9708bc0740a542685
Rafał <rafal.wrzesniak94@gmail.com>
2020-07-06 19:52:42 +0200

    Init commit

--- src/mymoviemanager/MoviesAndActors/Main.java
+++ src/mymoviemanager/MoviesAndActors/Main.java
@@ -3,6 +3,6 @@ package mymoviemanager.MoviesAndActors;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello there!");
     }
 }
