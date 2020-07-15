69a9eeb9a6da923b9d676a485df3322cbe7698b2
Rafał <rafal.wrzesniak94@gmail.com>
2020-07-15 23:41:55 +0200

    Changes due to new class creation

--- src/MoviesAndActors/Actor.java
+++ src/MoviesAndActors/Actor.java
@@ -9,44 +9,40 @@ import org.slf4j.LoggerFactory;
 public class Actor extends Person {
 
     private static final Logger logger = LoggerFactory.getLogger(Actor.class.getName());
-    private final List<Integer> playedInMovies = new ArrayList<>();
+    private final List<Movie> playedInMovies = new ArrayList<>();
 
     public Actor(String name, String surname, String nationality, LocalDate birthday, String imagePath) {
         super(name, surname, nationality, birthday, imagePath);
-        logger.debug("New actor created: {}", this.toString());
+        logger.info("New actor created: {}", this.toString());
     }
 
 
-    public List<Integer> getAllMoviesActorPlayedIn() {
+    public List<Movie> getAllMoviesActorPlayedIn() {
         return new ArrayList<>(playedInMovies);
     }
 
-    public boolean isPlayingIn(int movieId) {
-        return playedInMovies.contains(movieId);
+    public boolean isPlayingIn(Movie movie) {
+        return playedInMovies.contains(movie);
     }
 
-    public boolean addMovieActorPlayedIn(int movieId) {
-        if(movieId < 0) {
-            logger.warn("Wrong movie index: {}", movieId);
-            return false;
-        }
-        if(isPlayingIn(movieId)) {
-            logger.warn("Movie #{} already exists in: {}}", movieId, this.toString());
+    public boolean addMovieActorPlayedIn(Movie movie) {
+        if(isPlayingIn(movie)) {
+            logger.warn("Movie #{} already exists in: {}}", movie, this.toString());
             return false;
         } else {
-            playedInMovies.add(movieId);
-            logger.debug("Movie #{} added to: {}", movieId, this.toString());
+            playedInMovies.add(movie);
+            logger.debug("Movie #{} added to: {}", movie, this.toString());
             return true;
         }
     }
 
-    public boolean addSeveralMoviesToActor(List<Integer> moviesToAdd) {
+    public boolean addSeveralMoviesToActor(List<Movie> moviesToAdd) {
         if(moviesToAdd.size() < 1) {
             logger.warn("Empty list added as input to add several movies");
             return false;
         }
-        for (Integer integer : moviesToAdd) {
-            addMovieActorPlayedIn(integer);
+        for (Movie movie : moviesToAdd) {
+            addMovieActorPlayedIn(movie);
         }
         logger.debug("Several movies added to: {}", this.toString());
         return true;
--- src/MoviesAndActors/Director.java
+++ src/MoviesAndActors/Director.java
@@ -10,31 +10,27 @@ import java.util.List;
 public class Director extends Actor {
 
     private static final Logger logger = LoggerFactory.getLogger(Actor.class.getName());
-    private final List<Integer> directedMovies = new ArrayList<>();
+    private final List<Movie> directedMovies = new ArrayList<>();
 
     public Director(String name, String surname, String nationality, LocalDate birthday, String imagePath) {
         super(name, surname, nationality, birthday, imagePath);
-        logger.debug("New director created: {}", this.toString());
+        logger.info("New director created: {}", this.toString());
     }
 
-    public List<Integer> getAllMoviesDirectedBy() {
+    public List<Movie> getAllMoviesDirectedBy() {
         return new ArrayList<>(directedMovies);
     }
-    public boolean isDirecting(Integer movieId) {
-        return directedMovies.contains(movieId);
+    public boolean isDirecting(Movie movie) {
+        return directedMovies.contains(movie);
     }
 
-    public boolean addMovieDirectedBy(Integer movieId) {
-        if(movieId < 0) {
-            logger.warn("Wrong movie index: {}", movieId);
-            return false;
-    }
-        if(isDirecting(movieId)) {
-            logger.warn("Movie #{} already exists in: {}}", movieId, this.toString());
+    public boolean addMovieDirectedBy(Movie movie) {
+        if(isDirecting(movie)) {
+            logger.warn("Movie #{} already exists in: {}}", movie, this.toString());
             return false;
         } else {
-            directedMovies.add(movieId);
-            logger.debug("Movie #{} added to: {}", movieId, this.toString());
+            directedMovies.add(movie);
+            logger.debug("Movie #{} added to: {}", movie, this.toString());
             return true;
         }
     }
