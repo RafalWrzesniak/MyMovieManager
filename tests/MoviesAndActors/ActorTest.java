9a3bee68b2a15b8ba8376ad8758fe400654d6874
Rafał <rafal.wrzesniak94@gmail.com>
2020-07-15 23:38:59 +0200

    Updates in tests due to class changes

--- tests/MoviesAndActors/ActorTest.java
+++ tests/MoviesAndActors/ActorTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,42 +12,50 @@ import static org.junit.jupiter.api.Assertions.*;
 class ActorTest {
 
     Actor actor;
+    Movie most = new Movie("Most szpiegów", LocalDate.of(2015, 10, 16));
+    Movie birdman = new Movie("Birdman", LocalDate.of(2014, 8, 27));
+    Movie lotr = new Movie("Władca pierścieni: Drużyna Pierścienia", LocalDate.of(2001, 12, 13));
+    Movie deadpool = new Movie("Deadpool", LocalDate.of(2016, 1, 21));
 
     @BeforeEach
     void setUp() {
         actor = new Actor("Jack", "Sparrow", "Karaibian", LocalDate.of(1957, 6, 2), "E:\\xInne\\dk.jpg");
-        actor.addMovieActorPlayedIn(300);
-        actor.addMovieActorPlayedIn(2);
-        actor.addMovieActorPlayedIn(756);
     }
 
     @Test
     void getAllMoviesActorPlayedIn() {
-        List<Integer> tab = Arrays.asList(300, 2, 756);
+        actor.addMovieActorPlayedIn(most);
+        actor.addMovieActorPlayedIn(birdman);
+        actor.addMovieActorPlayedIn(lotr);
+        List<Movie> tab = Arrays.asList(most, birdman, lotr);
         assertArrayEquals(tab.toArray(), actor.getAllMoviesActorPlayedIn().toArray());
     }
 
 
     @Test
     void isPlayingIn() {
-        assertTrue(actor.isPlayingIn(2));
-        assertFalse(actor.isPlayingIn(32));
+        actor.addMovieActorPlayedIn(lotr);
+        assertTrue(actor.isPlayingIn(lotr));
+        assertFalse(actor.isPlayingIn(deadpool));
     }
 
 
     @Test
     void addMovieActorPlayedIn() {
-        actor.addMovieActorPlayedIn(200);
-        assertTrue(actor.isPlayingIn(200));
+        actor.addMovieActorPlayedIn(deadpool);
+        assertTrue(actor.isPlayingIn(deadpool));
     }
 
     @Test
     void addSeveralMoviesToActor() {
-        List<Integer> tab = Arrays.asList(100, 120, 140);
+        actor.addMovieActorPlayedIn(most);
+        actor.addMovieActorPlayedIn(birdman);
+        actor.addMovieActorPlayedIn(lotr);
+        List<Movie> tab = Arrays.asList(most, birdman, lotr);
         actor.addSeveralMoviesToActor(tab);
-        assertTrue(actor.isPlayingIn(100));
-        assertTrue(actor.isPlayingIn(120));
-        assertTrue(actor.isPlayingIn(140));
-        assertFalse(actor.isPlayingIn(240));
+        assertTrue(actor.isPlayingIn(most));
+        assertTrue(actor.isPlayingIn(birdman));
+        assertTrue(actor.isPlayingIn(birdman));
+        assertFalse(actor.isPlayingIn(deadpool));
     }
 }
--- tests/MoviesAndActors/DirectorTest.java
+++ tests/MoviesAndActors/DirectorTest.java
@@ -12,29 +12,36 @@ import static org.junit.jupiter.api.Assertions.*;
 class DirectorTest {
 
     Director director;
+    Movie most = new Movie("Most szpiegów", LocalDate.of(2015, 10, 16));
+    Movie birdman = new Movie("Birdman", LocalDate.of(2014, 8, 27));
+    Movie lotr = new Movie("Władca pierścieni: Drużyna Pierścienia", LocalDate.of(2001, 12, 13));
+    Movie deadpool = new Movie("Deadpool", LocalDate.of(2016, 1, 21));
+
 
     @BeforeEach
     void setUp() {
         director = new Director("Jack", "Sparrow", "Karaibian", LocalDate.of(1957, 6, 2), "E:\\xInne\\dk.jpg");
-        director.addMovieDirectedBy(300);
-        director.addMovieDirectedBy(2);
-        director.addMovieDirectedBy(756);
     }
+
     @Test
     void getAllMoviesDirectedBy() {
-        List<Integer> tab = Arrays.asList(300, 2, 756);
+        director.addMovieDirectedBy(most);
+        director.addMovieDirectedBy(birdman);
+        director.addMovieDirectedBy(lotr);
+        List<Movie> tab = Arrays.asList(most, birdman, lotr);
         assertArrayEquals(tab.toArray(), director.getAllMoviesDirectedBy().toArray());
     }
 
     @Test
     void isDirecting() {
-        assertTrue(director.isDirecting(300));
-        assertFalse(director.isDirecting(120));
+        director.addMovieDirectedBy(most);
+        assertTrue(director.isDirecting(most));
+        assertFalse(director.isDirecting(birdman));
     }
 
     @Test
     void addMovieDirectedBy() {
-        director.addMovieDirectedBy(100);
-        assertTrue(director.isDirecting(100));
+        director.addMovieDirectedBy(deadpool);
+        assertTrue(director.isDirecting(deadpool));
     }
 }
