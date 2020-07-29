9f6ac1cd49daa1f73faceb0d9415f413fe532800
Rafał <rafal.wrzesniak94@gmail.com>
2020-07-29 21:28:09 +0200

    Two new fields in interface - getId() and getAllFields()

--- src/MoviesAndActors/ContentType.java
+++ src/MoviesAndActors/ContentType.java
@@ -1,5 +1,9 @@
 package MoviesAndActors;
 
+import java.util.Map;
+
 public interface ContentType<T> extends Comparable<T> {
     boolean searchFor(String strToFind);
+    int getId();
+    Map<String, String> getAllFields();
 }
