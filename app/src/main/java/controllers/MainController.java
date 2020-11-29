c20c138d4ef3373f7c9f4fd24de86eaf0b5aca3e
Rafał <rafal.wrzesniak94@gmail.com>
2020-11-29 23:41:53 +0100

    Wrapped in flow pane on the app center into scroll pane

--- app/src/main/java/controllers/MainController.java
+++ app/src/main/java/controllers/MainController.java
@@ -10,22 +10,27 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.FlowPane;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
-
+@Slf4j
 public class MainController {
 
+    //    == fields ==
     ContentList<Actor> allActors;
     ContentList<Movie> allMovies;
     ContentList<Movie> moviesToWatch;
 
     @FXML public Button addContentMovies, addContentActors, addFolderWithMovies, addSingleMovie, addMovieFromWeb;
     @FXML public FlowPane flowPaneContentList = new FlowPane();
+    @FXML public ScrollPane scrollPane;
 
-
+    //    == init ==
     public void initialize() throws InterruptedException {
+        // load all data
         XMLOperator.ReadAllDataFromFiles readData = new XMLOperator.ReadAllDataFromFiles();
         readData.start();
         readData.join();
@@ -41,9 +46,39 @@ public class MainController {
         System.out.println(allActors);
         System.out.println(moviesToWatch);
 
+        // populate list
+        populateFlowPaneContentList(moviesToWatch);
+
+        // control speed of scroll pane
+        final double SPEED = 0.005;
+        scrollPane.getContent().setOnScroll(scrollEvent -> {
+            double deltaY = scrollEvent.getDeltaY() * SPEED;
+            scrollPane.setVvalue(scrollPane.getVvalue() - deltaY);
+        });
+
+
     }
 
+//    == methods ==
+    private void populateFlowPaneContentList(ContentList<Movie> contentList) {
+        flowPaneContentList.getChildren().clear();
+        for(int i =0; i < contentList.size(); i++) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml-views/movie-pane.fxml"));
+            Parent moviePane;
+            try {
+                moviePane = loader.load();
+            } catch (IOException e) {
+                log.warn("Failed to load fxml view in MoviePane");
+                continue;
+            }
+            MoviePane moviePaneController = loader.getController();
+            moviePaneController.setMovie(contentList.get(i));
+            flowPaneContentList.getChildren().add(moviePane);
+        }
 
+    }
+
+//    == fxml handling ==
     @FXML
     public void addContentList(ActionEvent actionEvent) {
         String message;
@@ -60,14 +95,12 @@ public class MainController {
     @FXML
     public void refreshMainFolder() throws IOException {
         System.out.println("Starting to refresh...");
-
-
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml-views/movie-pane.fxml"));
         Parent moviePane = loader.load();
         MoviePane moviePaneController = loader.getController();
         moviePaneController.setMovie(moviesToWatch.getById(25));
-
         flowPaneContentList.getChildren().add(moviePane);
+
 
     }
 
