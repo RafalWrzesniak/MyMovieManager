a49e6c6bdef7eca21a56c1ed08643cd7acb95ac5
Rafał <rafal.wrzesniak94@gmail.com>
2020-12-18 22:06:09 +0100

    New util class with static strings representing fxml names

---
+++ app/src/main/java/utils/PaneNames.java
@@ -0,0 +1,20 @@
+package utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.NONE)
+public final class PaneNames {
+
+    //  == constants ==
+    public static final String SINGLE_DIALOG = "single-dialog";
+    public static final String ACTOR_PANE = "actor-pane";
+    public static final String ACTOR_EDIT = "actor-edit";
+    public static final String ACTOR_DETAIL = "actor-detail";
+    public static final String MOVIE_PANE = "movie-pane";
+    public static final String MOVIE_EDIT = "movie-edit";
+    public static final String MOVIE_INFO = "movie-info";
+    public static final String MOVIE_DETAIL = "movie-detail";
+    public static final String MAIN_VIEW = "main-view";
+    public static final String SETTINGS = "settings";
+}
